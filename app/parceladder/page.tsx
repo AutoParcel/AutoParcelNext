1d0d9343558e4559254cebc63f271307dfd2b181
ThisAbhinav <this.abhinav26@gmail.com>
2023-10-22 12:33:15 +0530

    epic

--- app/parceladder/page.tsx
+++ app/parceladder/page.tsx
@@ -1,151 +1,59 @@
-"use client";
-import React, { useEffect, useRef } from "react";
+import React, { useState } from "react";
+import Webcam from "react-webcam";
 
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
-import { Button } from "@/components/ui/button";
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-});
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
 
-const opencamera = () => {
-  console.log("camera opened");
-};
+export default function WebcamCapture() {
+  const webcamRef: any = React.useRef(null);
+  const [image, setImage] = useState("");
 
-const ParcelAdder = () => {
-  const videoRef = useRef(null);
-  const photoRef = useRef<HTMLCanvasElement | null>(null);
+  const [facingMode, setFacingMode] = React.useState(FACING_MODE_USER);
 
-  useEffect(() => {
-    navigator.mediaDevices
-      .getUserMedia({
-        video: { width: 1920, height: 1080 },
-      })
-      .then((stream) => {
-        let video: any = videoRef.current;
-        video.srcObject = stream;
-        video.play();
-      });
-  }, [videoRef]);
-  const takePhoto = () => {
-    const width = 414;
-    const height = width / (16 / 9);
-    let video = videoRef.current;
-    let photo: any = photoRef.current;
-    photo.width = width;
-    photo.height = height;
-    let context = photo.getContext("2d");
-    context.drawImage(video, 0, 0, width, height);
+  const capture = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImage(imageSrc);
+  }, [webcamRef]);
+
+  let videoConstraints: MediaTrackConstraints = {
+    facingMode: facingMode,
+    width: 270,
+    height: 480,
   };
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      username: "",
-    },
-  });
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
-  }
+
+  const handleClick = React.useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
+
+  console.log(facingMode + videoConstraints);
+
   return (
     <>
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Parcel ID</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <FormField
-            control={form.control}
-            name="username"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Username</FormLabel>
-                <FormControl>
-                  <Input placeholder="shadcn" {...field} />
-                </FormControl>
-                <FormDescription>
-                  This is your public display name.
-                </FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button className={"m-4"} type="button" onClick={opencamera}>
-            Scan label
-          </Button>
-
-          <Button type="submit">Submit</Button>
-        </form>
-      </Form>
-      <div
-        style={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          width: "50vw",
-          height: "50vh",
-        }}
-      >
-        <div className="">
-          <video className="camera" ref={videoRef}></video>
+      <div className="webcam-container">
+        <div className="webcam-img">
+          {image === "" ? (
+            <Webcam
+              className="webcam"
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              videoConstraints={videoConstraints}
+              screenshotQuality={1}
+            />
+          ) : (
+            <img
+              src={image}
+              alt="Scan"
+              style={{ width: "500px", height: "auto" }}
+            />
+          )}
         </div>
-
-        <button
-          onClick={() => takePhoto()}
-          style={{
-            margin: "10px",
-            padding: "5px",
-          }}
-        >
-          Take photo
-        </button>
-        {photoRef && <canvas ref={photoRef}></canvas>}
+        <button onClick={handleClick}>Switch camera</button>
       </div>
     </>
   );
-};
-
-export default ParcelAdder;
+}
