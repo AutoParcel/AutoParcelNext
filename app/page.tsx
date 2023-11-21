69435defef1b36f091da784991969c4d6d5d78b4
ThisAbhinav <this.abhinav26@gmail.com>
2023-11-21 11:49:35 +0530

    main screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,24 @@
 import Link from "next/link";
-
+import { LuPackagePlus, LuPackageSearch } from "react-icons/lu";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <div className="flex justify-between w-full">
-        <div className="text-center p-40 w-1/2">
+      <div className="flex justify-around w-full text-2xl font-bold text-primary_black">
+        <div className="text-center">
+          <LuPackageSearch
+            size={200}
+            className="stroke-primary_yellow stroke-1"
+          />
           <Link href="/parcels">Parcel List</Link>
         </div>
-        <div className="text-center p-40 w-1/2">
+        <div className="text-center">
+          <LuPackagePlus
+            size={200}
+            className="stroke-primary_yellow stroke-1"
+          />
           <Link href="/parceladder">Add Parcel</Link>
         </div>
-      </div>
-      <div className="text-center p-20 mt-10">
-        <Link href="/about">About Us</Link>
       </div>
     </main>
   );
 }
-
-
