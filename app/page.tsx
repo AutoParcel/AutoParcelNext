6bbf8ba3c37e172cc325602f4a4eafd707a90edd
Jiya Agrawal <agrawal1jiya@gmail.com>
2023-11-21 11:29:18 +0530

    jiya commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,21 @@
 import Link from "next/link";
+
 export default function Home() {
   return (
-    <main className="flex flex-col items-center justify-between gap-5 m-10">
-      <Link href="/">Home</Link>
-      <Link href="/parceladder">Add new parcel</Link>
-      <Link href="/parcels">Parcels</Link>
-      <Link href="/about">About Us</Link>
+    <main className="flex flex-col items-center justify-center h-screen">
+      <div className="flex justify-between w-full">
+        <div className="text-center p-40 w-1/2">
+          <Link href="/parcels">Parcel List</Link>
+        </div>
+        <div className="text-center p-40 w-1/2">
+          <Link href="/parceladder">Add Parcel</Link>
+        </div>
+      </div>
+      <div className="text-center p-20 mt-10">
+        <Link href="/about">About Us</Link>
+      </div>
     </main>
   );
 }
+
+
--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -16,9 +16,8 @@ const TopNav = () => {
           AutoParcel
         </Link>
         <div className="flex gap-5 text-xs items-center">
-          <Link href="/">Home</Link>
-          <Link href="/parceladder">Add new parcel</Link>
-          <Link href="/parcels">Parcels</Link>
+          <Link href="/parceladder">Add parcel</Link>
+          <Link href="/parcels">Parcel List</Link>
           <Link href="/about">About Us</Link>
         </div>
       </div>
