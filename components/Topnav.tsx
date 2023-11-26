013fe6300609fc7823aeeb408499a1b82e6f3e57
ThisAbhinav <this.abhinav26@gmail.com>
2023-11-26 11:47:49 +0530

    s

--- components/Topnav.tsx
+++ components/Topnav.tsx
@@ -19,7 +19,7 @@ const TopNav = () => {
       <div className="flex gap-5">
         <div className="flex gap-5 text-xs items-center">
           <Link href="/parceladder">Add Parcel</Link>
-          <Link href="/parcels">Search Parcel</Link>
+          <Link href="/parcels">Parcels</Link>
           <Link href="/">Statistics</Link>
           <Link href="/about">About Us</Link>
         </div>
