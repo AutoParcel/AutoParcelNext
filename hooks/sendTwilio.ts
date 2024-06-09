df9a7ca553ecc361d874cdfb577d71b50552c6e0
ThisAbhinav <this.abhinav26@gmail.com>
2024-06-09 14:13:00 +0530

    fix: binary targets

--- hooks/sendTwilio.ts
+++ hooks/sendTwilio.ts
@@ -25,6 +25,7 @@ export default async function sendMessage(
     };
     const headers = {
       "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
     };
 
     const result = await axios
