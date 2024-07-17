dd873f9b32f74ab9bbbfb75c054bbf1e1389a8ae
ThisAbhinav <this.abhinav26@gmail.com>
2024-07-17 15:56:50 +0530

    test route

--- app/api/jiya/route.ts
+++ app/api/jiya/route.ts
@@ -1,4 +1,5 @@
 export async function GET(request: Request) {
      // in this file you can write the logic
+     console.log("Someone called me! cron job is running");
         return new Response("GET request received");
   }
